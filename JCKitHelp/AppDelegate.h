ab8b481801a8f5f736767316e1796720740da87c
Jia Miao <854569347@qq.com>
2016-09-22 22:02:25 +0800

    Initial Commit

---
+++ JCKitHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCKitHelp
+//
+//  Created by 贾淼 on 16/9/22.
+//  Copyright © 2016年 Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
